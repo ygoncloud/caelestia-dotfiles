a82519a37d786c9dfbc8171626d4639e597f9559
2 * r + 2 * t <61896496+soramanew@users.noreply.github.com>
2025-01-20 16:51:00 +1100

    monitor-scheme: remove unnecessary var

--- scripts/monitor-scheme.fish
+++ scripts/monitor-scheme.fish
@@ -2,7 +2,6 @@
 
 set -q XDG_CACHE_HOME && set cache $XDG_CACHE_HOME || set cache ~/.cache
 set scheme_path $cache/caelestia/scheme/current.txt
-set schemes ~/.config/hypr/scheme
 
 inotifywait -e 'close_write,moved_to,create' -m (dirname $scheme_path) | while read dir events file
     test "$dir$file" = $scheme_path && hyprctl reload
