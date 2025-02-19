b9babf11e2f44b04e19d0ace671a1a6af84845f3
2 * r + 2 * t <61896496+soramanew@users.noreply.github.com>
2025-02-19 15:27:00 +1100

    use new scheme backend

--- scripts/gen-scheme.fish
+++ scripts/gen-scheme.fish
@@ -1,11 +1,17 @@
 #!/bin/fish
 
-set -q XDG_CACHE_HOME && set cache $XDG_CACHE_HOME || set cache ~/.cache
-set scheme_path $cache/caelestia/scheme/current.txt
-
-set scheme mocha
-if test -f $scheme_path
-    set -l current_scheme (cat $scheme_path)
-    test -f ~/.config/hypr/scheme/$current_scheme.conf && set scheme $current_scheme
+function read-scheme -a scheme_path
+    cat $scheme_path | while read line
+        set -l colour (string split ' ' $line)
+        echo "\$$colour[1] = $colour[2]"
+    end
 end
-echo "source = ~/.config/hypr/scheme/$scheme.conf" > ~/.config/hypr/scheme/current.conf
+
+set -q XDG_CACHE_HOME && set -l cache $XDG_CACHE_HOME || set -l cache ~/.cache
+set -l scheme_path $cache/caelestia/scheme/current.txt
+
+if test -f $scheme_path
+    read-scheme $scheme_path > ~/.config/hypr/scheme/current.conf
+else
+    cp -f ~/.config/hypr/scheme/default.conf ~/.config/hypr/scheme/current.conf
+end
