6ca12dc56f433c1527f81702e2f58b29e76e3b18
2 * r + 2 * t <61896496+soramanew@users.noreply.github.com>
2025-03-09 23:27:08 +1100

    hyprlock: multiple styles
    Choose style using config

--- scripts/gen-hyprlock.fish
+++ scripts/gen-hyprlock.fish
@@ -2,18 +2,27 @@
 
 set -l src (dirname (status filename))
 
-cp $src/templates/hyprlock-base.conf $src/../hyprlock.conf
+set -q XDG_CONFIG_HOME && set -l config $XDG_CONFIG_HOME/caelestia || set -l config $HOME/.config/caelestia
 
-for monitor in (hyprctl monitors -j | jq -r '.[] | "\(.name) \(.width) \(.height)"')
-    set -l monitor (string split ' ' $monitor)
-    set -l name $monitor[1]
-    set -l width $monitor[2]
-    set -l height $monitor[3]
+cp $src/templates/hyprlock/base.conf $src/../hyprlock.conf
 
-    set -l tmp (mktemp)
-    cp $src/templates/hyprlock-monitor.conf $tmp
-    sed -n 's/.*{{ \(.*\) }}.*/\1/p' $src/templates/hyprlock-monitor.conf | while read expr
-        sed -i "s|{{ $expr }}|$(echo "echo $expr" | .)|g" $tmp
+test -f $config/hypr.json && set -l style (jq -r '.hyprlock.style' $config/hypr.json)
+test -f "$src/templates/hyprlock/$style/main.conf" || set -l style round
+
+cat $src/templates/hyprlock/$style/main.conf >> $src/../hyprlock.conf
+
+if test -f $src/templates/hyprlock/$style/per-monitor.conf
+    for monitor in (hyprctl monitors -j | jq -r '.[] | "\(.name) \(.width) \(.height)"')
+        set -l monitor (string split ' ' $monitor)
+        set -l name $monitor[1]
+        set -l width $monitor[2]
+        set -l height $monitor[3]
+
+        set -l tmp (mktemp)
+        cp $src/templates/hyprlock/$style/per-monitor.conf $tmp
+        sed -n 's/.*{{ \(.*\) }}.*/\1/p' $src/templates/hyprlock/$style/per-monitor.conf | while read expr
+            sed -i "s|{{ $expr }}|$(echo "echo $expr" | .)|g" $tmp
+        end
+        cat $tmp >> $src/../hyprlock.conf
     end
-    cat $tmp >> $src/../hyprlock.conf
 end
