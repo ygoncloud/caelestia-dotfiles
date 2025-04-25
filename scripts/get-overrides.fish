da039bcbb01ce4d0f089073d006aa10e0ddc7645
2 * r + 2 * t <61896496+soramanew@users.noreply.github.com>
2025-04-25 18:36:08 +1000

    rename: gaps -> aesthetic

--- scripts/get-overrides.fish
+++ scripts/get-overrides.fish
@@ -4,7 +4,7 @@ set -l src (dirname (status filename))
 set -q XDG_CONFIG_HOME && set -l config $XDG_CONFIG_HOME/caelestia || set -l config $HOME/.config/caelestia
 
 test -f $config/hypr.json && set -l style (jq -r '.hyprland.style' $config/hypr.json)
-test -f "$src/templates/hyprland/$style.conf" || set -l style gaps
+test -f "$src/templates/hyprland/$style.conf" || set -l style aesthetic
 
 set -l template $src/templates/hyprland/$style.conf
 set -l dest $src/../hyprland/overrides.conf
